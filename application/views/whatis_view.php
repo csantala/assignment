9f4de9f8456218928ca3a038f91e6fcb908705e7
Chris Santala <csantala@gmail.com>
2013-12-04 04:54:57 -0500

    what is updates

--- application/views/whatis_view.php
+++ application/views/whatis_view.php
@@ -58,12 +58,15 @@
                 </div>
                 <div class="widget-body">
                     <div class="row-fluid">
-                    	<h5>Electronic Learning Aid?</h5>
-                    	<p>This is a tool which allows an assignment to be generated, passed to any number of students, and have their work tracked and reported back to you.</p>
+                    	<h5>Electronic Learning Aid</h5>
+                    	<p>This is a system which manages the creation and completion of an assignment conducted by an instructor with any number of students.<p><p>  Essentially, the instructor creates an assignment, passes its URL to the students who then complete the assignment while logging their work. Students return their synopsis to the instructor in the form of a report.  Links to each report appear in the instructor's dashboard. A report can be commented on and is always available at its unique hashed URL.</p>
                     	<h5>Real Time Synopsis Editor</h5>
-                    	<p>Students use the synopsis editor to log each step they take to complete the assignment. This is useful to determine trouble spots during the learning process and it also provides the student with a moment to pause and reflect as they record the tasks conducted in the completion of the assignment.</p>
-                    	<h5>How do I use this?</h5>
-                    	<p>Simply generate an assignment, send it to your students, and then monitor your assignment dashboard as they complete their work.  You can view and comment on student reports with links from the dashboard.<p>
+
+                    	<p>Students use the synopsis editor to document the tasks they take to complete the assignment. The editor is served with a unique hashed URL meaning that it may always be returned to in the future for further work - it's best to bookmark this page.</p>
+                    	<h5>Usage</h5>
+                    	<p>Simply <a href="<?php echo site_url();?>create">create an assignment</a>, send its URL to your students, and then bookmark/monitor your assignment dashboard as they complete their work.  You can view and comment on student reports with links from the dashboard. </p>
+            			<h5>Synopsis mini</h5>
+            			An online synopsis editor and reporter (minus the assignment functionality of this site) is available at <a href="http://synopsis.ablitica.com" target="_blank">synopsis.ablitica.com</a>
  					 </div>
                 </div>
             </div>
